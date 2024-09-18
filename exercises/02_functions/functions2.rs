4f8085a281cd40e0f85561327ae683a55078fc3a
Leyla Sena Kılınç <81108279+lsenakilinc@users.noreply.github.com>
2024-09-18 14:17:55 +0300

    Update functions2.rs

--- exercises/02_functions/functions2.rs
+++ exercises/02_functions/functions2.rs
@@ -1,5 +1,6 @@
 // TODO: Add the missing type of the argument `num` after the colon `:`.
-fn call_me(num:) {
+fn call_me(num:u32) {
+    // u32=unsigned, i32=signed
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
