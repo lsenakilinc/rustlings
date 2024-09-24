d6b289669882f0aab56ffb1b5d5979c117a69e58
Leyla Sena Kılınç <81108279+lsenakilinc@users.noreply.github.com>
2024-09-24 16:44:15 +0300

    Update if3.rs

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -1,13 +1,17 @@
 fn animal_habitat(animal: &str) -> &str {
     // TODO: Fix the compiler error in the statement below.
     let identifier = if animal == "crab" {
+        //"Beach"
         1
     } else if animal == "gopher" {
-        2.0
+        //"Burrow"
+        2
     } else if animal == "snake" {
+        //"Desert"
         3
     } else {
-        "Unknown"
+        //"Unknown"
+        4
     };
 
     // Don't change the expression below!
@@ -18,14 +22,23 @@ fn animal_habitat(animal: &str) -> &str {
     } else if identifier == 3 {
         "Desert"
     } else {
-        "Unknown"
+        "Dinasour"
     }
 }
 
 fn main() {
-    // You can optionally experiment here.
+       
+    let result1 = animal_habitat("gopher");
+    let result2 = animal_habitat("snake");
+    let result3 = animal_habitat("crab");
+    let result4 = animal_habitat("g");
+    
+        
+    println!("Result for 'gopher': {}", result1);  
+    println!("Result for 'snake': {}", result2);  
+    println!("Result for 'crab': {}", result3);  
+    println!("Result for 'g': {}", result4);  
 }
-
 // Don't change the tests!
 #[cfg(test)]
 mod tests {
