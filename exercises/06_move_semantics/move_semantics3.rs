5113f67686fb58c202afbf59e1217a42a2b31a1d
Leyla Sena Kılınç <81108279+lsenakilinc@users.noreply.github.com>
2024-10-16 16:52:54 +0300

    Update move_semantics3.rs

--- exercises/06_move_semantics/move_semantics3.rs
+++ exercises/06_move_semantics/move_semantics3.rs
@@ -1,12 +1,16 @@
 // TODO: Fix the compiler error in the function without adding any new line.
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
 
     vec
 }
 
 fn main() {
-    // You can optionally experiment here.
+    let vec = vec![22, 44, 66]; // Sample vector
+    let filled_vec = fill_vec(vec); // Call the function to fill the vector
+
+    // Print the result.
+    println!("Filled vector: {:?}", filled_vec); // Use debug formatting to print the vector
 }
 
 #[cfg(test)]
