46a9f75ee21e1aa77d0d4b89a5566dfef9099492
Leyla Sena Kılınç <81108279+lsenakilinc@users.noreply.github.com>
2024-10-16 17:17:59 +0300

    Update move_semantics5.rs

--- exercises/06_move_semantics/move_semantics5.rs
+++ exercises/06_move_semantics/move_semantics5.rs
@@ -4,21 +4,22 @@
 // removing references (the character `&`).
 
 // Shouldn't take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
+//since string is not copy type it shouldn't take ownership so that we can be able to use it later on
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
+fn string_uppercase(mut data: String) {
     data = data.to_uppercase();
-
+//
     println!("{data}");
 }
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data);
 
-    string_uppercase(&data);
+    string_uppercase(data);
 }
