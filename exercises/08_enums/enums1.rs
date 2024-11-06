b2b701e6a8ba2dfb1a6ec36790bf6ddaed4e3e8c
Leyla Sena Kılınç <81108279+lsenakilinc@users.noreply.github.com>
2024-11-06 13:42:44 +0300

    Update enums1.rs

--- exercises/08_enums/enums1.rs
+++ exercises/08_enums/enums1.rs
@@ -1,12 +1,17 @@
 #[derive(Debug)]
 enum Message {
+    Resize{w: u32, h: u32},
+    Move{x : i32 , y: i32 },
+    Echo {String},
+    ChangeColor{r: u8, g: u8, b: u8 },
+    Quit,
+    
     // TODO: Define a few types of messages as used below.
 }
-
 fn main() {
-    println!("{:?}", Message::Resize);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Resize { width: 800, height: 600 });
+    println!("{:?}", Message::Move { x: 10, y: 20 });
+    println!("{:?}", Message::Echo(String::from("Hello, world!")));
+    println!("{:?}", Message::ChangeColor { r: 255, g: 165, b: 0 });
     println!("{:?}", Message::Quit);
 }
